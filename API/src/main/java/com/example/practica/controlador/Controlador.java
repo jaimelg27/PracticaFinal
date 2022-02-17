8dac01fb5867a54c57188337d1417908209cbfd1
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-02-17 19:10:22 +0100

    Controller update

--- API/src/main/java/com/example/practica/controlador/Controlador.java
+++ API/src/main/java/com/example/practica/controlador/Controlador.java
@@ -1,5 +1,23 @@
 package com.example.practica.controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.practica.repositorio.ClienteRepository;
+import com.example.practica.repositorio.ProductoRepository;
+import com.example.practica.repositorio.VentaRepository;
+
+@Controller
+
 public class Controlador {
+	
+	@Autowired
+	private ClienteRepository clienterepo;
+	
+	@Autowired
+	private ProductoRepository productorepo;
+	
+	@Autowired
+	private VentaRepository ventarepo;
 
 }
