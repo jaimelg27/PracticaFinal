ab1c7e5858cb2bd85f436caac3b306170fc1c553
Jaime López <61801163+jaimelg27@users.noreply.github.com>
2022-02-18 17:45:38 +0100

    Service update

--- API/src/main/java/com/example/practica/servicio/VentaService.java
+++ API/src/main/java/com/example/practica/servicio/VentaService.java
@@ -1,10 +1,13 @@
 package com.example.practica.servicio;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.practica.modelo.Productos;
 import com.example.practica.modelo.Ventas;
 import com.example.practica.repositorio.VentaRepository;
 
@@ -12,13 +15,32 @@ import com.example.practica.repositorio.VentaRepository;
 public class VentaService {
 	
 	@Autowired
-	private VentaRepository ventaRepository;
+	private VentaRepository ventarepo;
 	
-	public List<Ventas> consulta(){
-		return ventaRepository.findAll();
+	//Añadir venta al repositorio
+	public Ventas create(Ventas v) {
+		return ventarepo.save(v);
 	}
 	
+	// PENDIENTE Consultar ventas por fechas
+	public Optional<Ventas> searchbyDate(Date on, Date off){
+		return ventarepo.
+	}
 	
+	// PENDIENTE Consultar ventas por clientes
+	public Optional<Ventas> searchbyClientNIF(String NIF){
+		
+	}
+	
+	public Optional<Ventas> searchbyID(int id){
+		return ventarepo.findById(id);
+	}
+	
+	//Eliminar producto por ID
+	public void deletebyId(int id) {
+		Ventas v=searchbyID(id).get();
+		ventarepo.delete(v);
+	}
 	
 
 }
